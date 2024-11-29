6eb580efe6577b19d3665294be97273cae567083
Erik R. Yverling <erik.r@yverling.se>
2024-11-29 16:45:54 +0100

    Bump dependencies and remove versions plugin

--- KotlinMultiplatformUI/build.gradle.kts
+++ KotlinMultiplatformUI/build.gradle.kts
@@ -3,18 +3,4 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.android) apply false
-    alias(libs.plugins.versions)
-}
-
-tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate.version)
-    }
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
 }
